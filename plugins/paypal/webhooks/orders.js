ed4f6b07a9811b7cf31f8a2d3f4f70c994b7da51
snoryder8019 <snoryder8019@gmail.com>
2023-12-07 15:03:41 -0700

    hook

--- plugins/paypal/webhooks/orders.js
+++ plugins/paypal/webhooks/orders.js
@@ -8,7 +8,9 @@ const checkouts = async (req, res) => {
         // Handle the webhook event here
         console.log('Webhook event received:', req.body);
         const customId = req.body.resource.purchase_units[0].custom_id;
-        console.log('Webhook event parsed by scott:', 'custom id: ',customId);
+        const customId2 = req.body.resource.purchase_units[0].custom_id2;
+        const status = req.body.status;
+        console.log('Webhook event parsed by scott:', 'custom id: ',customId,'custom id 2: ',customId2, 'status:', status);
 
         // Process the received data
         // For example, updating a database, sending notifications, etc.

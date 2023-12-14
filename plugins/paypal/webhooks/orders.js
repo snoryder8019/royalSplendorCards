17e8db4f0e57f9d6a9866095a59067d1296ecfce
snoryder8019 <snoryder8019@gmail.com>
2023-12-14 15:06:43 -0700

    mkay

--- plugins/paypal/webhooks/orders.js
+++ plugins/paypal/webhooks/orders.js
@@ -8,10 +8,11 @@ const checkouts = async (req, res) => {
     const allofIt = req.body;
     const status = req.body.resource.status;     
     const resourceType =  req.body.resource_type;     
-    const eventType =  req.body.event_type;     
+    const eventType =  req.body.event_type;   
+    const amount = req.body.resource.amount.value 
     
     console.log(`event received:\nOrderId: ${orderId}\nEventType: ${eventType}\nStatus: ${status}`);
-console.log(`all of it: ${allofIt}`)
+console.log(`purchase amount: ${amount}`)
         res.status(200).send('Event received');
     } catch (error) {
         console.error('Error processing the webhook event:', error);

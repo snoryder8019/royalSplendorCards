62ebcc814576c1642aa7c42dc9bfbfc967877b17
snoryder8019 <snoryder8019@gmail.com>
2023-12-14 14:36:40 -0700

    push data

--- plugins/paypal/webhooks/orders.js
+++ plugins/paypal/webhooks/orders.js
@@ -4,10 +4,20 @@ const router = express.Router();
 const checkouts = async (req, res) => {
     try {
     //mongo here
-        console.log('Webhook event received:', req.body);
-        const customId = req.body.resource.purchase_units[0].custom_id;    
+        console.log('Webhook event received:',);
+        const customId = req.body.resource.purchase_units[0].custom_id;
         const status = req.body.resource.status;     
-        console.log('Webhook event parsed by scott:', 'custom id: ',customId, 'status:', status);
+        const resourceType =  req.body.resource_type;     
+        const eventType =  req.body.event_type;     
+        const status = req.body.resource.status;     
+
+if (customId === null || customId === undefined) {
+    // Custom ID is null or undefined, you can move on here
+    console.log('Webhook event parsed by scott:',  'status:', status, 'event type:',eventType, 'resource type:', resourceType);
+} else {
+    console.log('Webhook event parsed by scott:', 'custom id: ',customId, 'status:', status);
+    // Custom ID has a value, you can continue with your script
+}
         res.status(200).send('Event received');
     } catch (error) {
         console.error('Error processing the webhook event:', error);

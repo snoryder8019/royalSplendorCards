aaaa77db53fdf306d601e828bddc5cce70485a0f
snoryder8019 <snoryder8019@gmail.com>
2023-12-14 14:50:05 -0700

    mkay

--- plugins/paypal/webhooks/orders.js
+++ plugins/paypal/webhooks/orders.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const checkouts = async (req, res) => {
     try {
     //mongo here
-    
+    const allofIt = req.body;
     const status = req.body.resource.status;     
     const resourceType =  req.body.resource_type;     
     const eventType =  req.body.event_type;     
     
-    console.log(`event received:\n resource type: ${resourceType}\neventType: ${eventType}\nstatus: ${status}`);
-
+    console.log(`event received:\nResource type: ${resourceType}\nEventType: ${eventType}\nStatus: ${status}`);
+console.log(`all of it: ${allofIt}`)
         res.status(200).send('Event received');
     } catch (error) {
         console.error('Error processing the webhook event:', error);

9083f6162d02e87f38d24e1697337ee81d636aa8
snoryder8019 <snoryder8019@gmail.com>
2023-12-21 20:40:32 -0700

    new hooks

--- plugins/paypal/webhooks/orders.js
+++ plugins/paypal/webhooks/orders.js
@@ -5,34 +5,40 @@ const {initiatePaypalOrder,updatePaypalOrder} = require('../dbFunctions')
 
 const checkouts = async (req, res) => {
     try {
-    //mongo here
-    const resourceType =  req.body.resource_type;     
-    // const amount = req.body.resource.amount.value 
-    const eventType =  req.body.event_type;   
-    if(eventType=="CHECKOUT.ORDER.COMPLETED"){
-        console.log(`fire finalization`)
-        initiatePaypalOrder;
-        const orderId = req.body.id
-        const payerEmail = req.body.resource.payer.email_address;
-        const payerId = req.body.resource.payer.payer_id;
-    console.log(`email:${payerEmail} id:${payerId}\nOrder Id: ${orderId}`)
-    }
-    if(eventType=="CHECKOUT.ORDER.APPROVED"){
-  
-        const orderId = req.body.id
-        const status = req.body.resource.status;     
-        const payerId = req.body.resource.payer.payer_id;
-        const payerEmail = req.body.resource.payer.email_address;
-        console.log(`event received:\nOrderId: ${orderId}\nEventType: ${eventType}\nStatus: ${status}`);
-        console.log(`id: ${payerId}, email: ${payerEmail} `)
-        updatePaypalOrder("paypalCompleted","true")
-        
-    }else{
-    const orderId = req.body.id
-    const summary = req.body.summary;
-    console.log(`other hooks: ${eventType}\nsummary: ${summary}, Order Id: ${orderId}`)
-}
+        const eventType = req.body.event_type;
+        const orderId = req.body.id; // Order ID is common for all events
 
+        if (eventType == "CHECKOUT.ORDER.COMPLETED") {
+            console.log(`Fire finalization`);
+            // Call your order finalization logic here
+
+            // Extract payer information
+            const payerEmail = req.body.resource.payer.email_address;
+            const payerId = req.body.resource.payer.payer_id;
+            console.log(`Email: ${payerEmail}, ID: ${payerId}, Order ID: ${orderId}`);
+
+            // Extract purchase units information
+            const purchaseUnits = req.body.resource.purchase_units;
+            if (purchaseUnits) {
+                purchaseUnits.forEach((unit, index) => {
+                    console.log(`Purchase Unit ${index + 1}:`);
+                    console.log(`Description: ${unit.description}`);
+                    console.log(`Amount: ${unit.amount.value} ${unit.amount.currency_code}`);
+                    // Add more fields as needed
+                });
+            }
+        } else if (eventType == "CHECKOUT.ORDER.APPROVED") {
+            const customId = req.body.resource.purchase_units[0]?.custom_id;
+            const status = req.body.resource.status;
+            const payerId = req.body.resource.payer.payer_id;
+            const payerEmail = req.body.resource.payer.email_address;
+            console.log(`Event received: OrderId: ${orderId}, EventType: ${eventType}, Status: ${status}, Custom Id: ${customId}`);
+            console.log(`ID: ${payerId}, Email: ${payerEmail}`);
+            updatePaypalOrder(orderId, {"paypalCompleted": "true"});
+        } else {
+            const summary = req.body.summary;
+            console.log(`Other hooks: ${eventType}, Summary: ${summary}, Order ID: ${orderId}`);
+        }
 
         res.status(200).send('Event received');
     } catch (error) {
@@ -40,6 +46,7 @@ const checkouts = async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 };
+
 
 
 module.exports = checkouts;

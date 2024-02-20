17181ff0356ea3f0719288a3af5c3feb682b9bb0
snoryder8019 <snoryder8019@gmail.com>
2024-02-20 10:29:19 -0700

    fixed resolution on fonts

--- public/javascripts/exportPDF.js
+++ public/javascripts/exportPDF.js
@@ -12,26 +12,28 @@ function convertJPGtoPNG(jpgSrc) {
     download(pngSrc, 'converted_image.png');
   };
 }
-//
-function exportHighResolutionImage(selector, fileName,orientation) {
+
+function exportHighResolutionImage(selector, fileName, orientation) {
   let canvasWidth;
   let canvasHeight;
-  if(orientation == "horizontal"){
-     canvasWidth = 1125; // Width in pixels canvasHeight = 675; // Height in pixels
+  const screenResolution = window.devicePixelRatio || 1; // Get screen resolution
 
+  if (orientation == "horizontal") {
+    canvasWidth = 1125; // Width in pixels
+    canvasHeight = 675; // Height in pixels
+  } else if (orientation == "vertical") {
+    canvasHeight = 1125; // Width in pixels
+    canvasWidth = 675; // Height in pixels
   }
-  if(orientation == "vertical"){
-   canvasHeight = 1125; // Width in pixels
-canvasWidth = 675; // Height in pixels
 
-  }
   const element = document.querySelector(selector);
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
-  const scaleFactor = 300 / 96; // Convert screen resolution (96 DPI) to 300 DPI
+  const scaleFactor = 300 / 96 * screenResolution; // Convert screen resolution to scale factor
   canvas.width = canvasWidth * scaleFactor;
   canvas.height = canvasHeight * scaleFactor;
   ctx.scale(scaleFactor, scaleFactor);
+
   html2canvas(element, { canvas: canvas }).then(canvas => {
     download(canvas.toDataURL('image/png'), fileName);
   });
@@ -46,74 +48,76 @@ function download(dataUrl, fileName) {
   document.body.removeChild(a);
 }
 
-function exportFrontFrameToPDF(userName,orientation) {
+function exportFrontFrameToPDF(userName, orientation) {
   const element = document.querySelector('.cardFrontFrame');
   let canvasWidth;
   let canvasHeight;
-  if(orientation == "horizontal"){
-     canvasWidth = 1125; // Width in pixels
- canvasHeight = 675; // Height in pixels
+  const screenResolution = window.devicePixelRatio || 1; // Get screen resolution
 
+  if (orientation == "horizontal") {
+    canvasWidth = 1125; // Width in pixels
+    canvasHeight = 675; // Height in pixels
+  } else if (orientation == "vertical") {
+    canvasWidth = 675; // Width in pixels
+    canvasHeight = 1125; // Height in pixels
   }
-  if(orientation == "vertical"){
-    canvasWidth = 675; // Height in pixels
-     canvasHeight = 1125; // Width in pixels
 
-  }
-exportHighResolutionImage('.cardFrontFrame',`card_front_${userName}.png`,orientation)
+  exportHighResolutionImage('.cardFrontFrame', `card_front_${userName}.png`, orientation);
+
   html2pdf()
     .from(element)
     .set({
       margin: [0, 0, 0, 0],
       filename: `card_front_${userName}.pdf`,
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2, width: canvasWidth, height: canvasHeight },
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: { scale: screenResolution, width: canvasWidth, height: canvasHeight },
       jsPDF: { unit: 'px', format: [canvasWidth, canvasHeight] }
     })
     .save();
 }
-//
-function exportBackFrameToPDF(userName,orientation) {
-  console.log(orientation)
+
+function exportBackFrameToPDF(userName, orientation) {
   const element = document.querySelector('.cardBackFrame');
   let canvasWidth;
   let canvasHeight;
-  if(orientation == "horizontal"){
-     canvasWidth = 1125; // Width in pixels
- canvasHeight = 675; // Height in pixels
+  const screenResolution = window.devicePixelRatio || 1; // Get screen resolution
 
+  if (orientation == "horizontal") {
+    canvasWidth = 1125; // Width in pixels
+    canvasHeight = 675; // Height in pixels
+  } else if (orientation == "vertical") {
+    canvasWidth = 675; // Width in pixels
+    canvasHeight = 1125; // Height in pixels
   }
-  if(orientation == "vertical"){
-    canvasWidth = 675; // Height in pixels
-     canvasHeight = 1125; // Width in pixels
 
-  }
-  exportHighResolutionImage('.cardBackFrame',`card_back_${userName}.png`,orientation)
+  exportHighResolutionImage('.cardBackFrame', `card_back_${userName}.png`, orientation);
+
   html2pdf()
     .from(element)
     .set({
       margin: [0, 0, 0, 0],
       filename: `card_back_${userName}.pdf`,
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 1, width: canvasWidth, height: canvasHeight },
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: { scale: screenResolution, width: canvasWidth, height: canvasHeight },
       jsPDF: { unit: 'px', format: [canvasWidth, canvasHeight] }
     })
     .save();
 }
+
 function exportUserDataToPDF(userName) {
   const element = document.querySelector('.orderData');
   const canvasWidth = 1125; // Width in pixels
   const canvasHeight = 675; // Height in pixels
- // exportHighResolutionImage('.orderData',`order_data_${userName}.png`)
+  const screenResolution = window.devicePixelRatio || 1; // Get screen resolution
+
   html2pdf()
     .from(element)
     .set({
       margin: [0, 0, 0, 0],
       filename: `order_data_${userName}.pdf`,
       //image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2, width: canvasWidth, height: canvasHeight },
+      html2canvas: { scale: 2 * screenResolution, width: canvasWidth, height: canvasHeight },
       jsPDF: { unit: 'px', format: [canvasWidth, canvasHeight] }
     })
     .save();
 }
-

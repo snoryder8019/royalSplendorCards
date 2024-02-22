a48a308d1c5e559ac40df4592d95ab19bfb12b84
snoryder8019 <snoryder8019@gmail.com>
2024-02-22 08:54:19 -0700

    redundent gitingnores, removed A enlarged

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ async function startApp() {
  
   app.use('/', indexRouter);
   app.use('/admin', adminRouter);
-  app.use('/viewBuy', viewBuyRouter);
+
   app.use('/', exporterRoute);
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {
